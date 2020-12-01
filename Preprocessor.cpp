9b17c1e79a14c0d7997046ee1ac7c6ef575f1c8f
Wei Guo <wei.parker.guo.sg@gmail.com>
2020-12-01 10:30:16 +0800

    [PREPROCESSOR]implemented adj rule and freq rule extraction.

--- Preprocessor.cpp
+++ Preprocessor.cpp
@@ -40,7 +40,18 @@ bool preprocessor::is_equal(tile& a, tile& b) {
 		&& a.map[8] == b.map[8];
 }
 
-void preprocessor::retrieve_tiles(std::vector<tile>& tiles, bitmap::bitmap bm) {
+bool preprocessor::is_equal(adj_rule& a, adj_rule& b) {
+	return a.first == b.first && a.second == b.second;
+}
+
+bool preprocessor::is_in(std::vector<adj_rule>& rules, adj_rule& rule) {
+	for (adj_rule r : rules) {
+		if (is_equal(r, rule)) return true;
+	}
+	return false;
+}
+
+void preprocessor::retrieve_tiles(std::vector<tile>& tiles, tile_pool& t_pool, bitmap::bitmap bm) {
 	int height = bm.size();
 	int width = bm[0].size();
 	for (int i = 0; i < height; i++) {
@@ -49,17 +60,60 @@ void preprocessor::retrieve_tiles(std::vector<tile>& tiles, bitmap::bitmap bm) {
 			tile new_tile;
 			for (int x = 0; x < 3; x++) {
 				for (int y = 0; y < 3; y++) {
-					new_tile.map[x * 3 + y] = bm[i + x][j + y];
+					//note we treat the bitmap as infinite tiling here by adding mods
+					new_tile.map[x * 3 + y] = bm[(i + x) % height][(j + y) % width];
 				}
 			}
-			//compare it with the list, if not repeated add to it.
-			bool repeat = false;
+			//compare it with the list, if not repeated add to it, if repeated update the freq rule
+			bool repeat_index = INFINITY;
+			int ti = 0;
 			for (tile t : tiles) {
-				if (is_equal(new_tile, t)) repeat = true;
+				if (is_equal(new_tile, t)) {
+					repeat_index = ti;
+					t.freq += 1;
+					break;
+				}
+				ti += 1;
 			}
-			if (!repeat) {
+			//new tile to add
+			if (repeat_index == INFINITY) {
 				new_tile.index = tiles.size();
+				new_tile.freq = 1;
+				t_pool[i][j] = new_tile.index;
 				tiles.push_back(new_tile);
+			}
+			//old tile to update
+			else t_pool[i][j] = repeat_index;
+		}
+	}
+}
+
+void preprocessor::retrieve_adj_rules(std::vector<tile>& tiles, tile_pool& t_pool) {
+	//walk through each tile and update adj rules, treat map as infinitely tiling
+	int pool_height = t_pool.size();
+	int pool_width = t_pool[0].size();
+	for (tile t : tiles) {
+		for (int i = 0; i < pool_height; i++) {
+			for (int j = 0; j < pool_width; j++) {
+				if (t.index == t_pool[i][j]) {
+					adj_rule up_rule;
+					int up_index = i - 1;
+					if (up_index < 0) up_index += pool_height;
+					up_rule.first = t_pool[up_index][j];
+					adj_rule down_rule;
+					down_rule.first = t_pool[(i + 1) % pool_height][j];
+					adj_rule left_rule;
+					int left_index = j - 1;
+					if (left_index < 0) left_index += pool_width;
+					left_rule.first = t_pool[i][left_index];
+					adj_rule right_rule;
+					right_rule.first = t_pool[i][(j + 1) % pool_width];
+					//update only when necessary
+					if (!is_in(t.adj_list, up_rule)) t.adj_list.push_back(up_rule);
+					if (!is_in(t.adj_list, down_rule)) t.adj_list.push_back(down_rule);
+					if (!is_in(t.adj_list, left_rule)) t.adj_list.push_back(left_rule);
+					if (!is_in(t.adj_list, right_rule)) t.adj_list.push_back(right_rule);
+				}
 			}
 		}
 	}
--- Preprocessor.h
+++ Preprocessor.h
@@ -4,9 +4,18 @@
 #include "bitmap.h"
 
 namespace preprocessor {
+	//define a pair of <tile index, direction> as adjacency rules
+	//directions follow clockwisely: 0 - up 1 - right 2 - down 3 - left
+	typedef std::pair<int, int> adj_rule;
+	//define a tile pool as cache for constant time compatibility check and update
+	//not we use 2d vector here for convinience and readability
+	typedef std::vector<std::vector<int>> tile_pool;
+
 	//define tiles, size as 3x3
 	struct tile {
 		int index;
+		int freq;
+		std::vector<adj_rule> adj_list;
 		bool map[9];
 	};
 
@@ -16,9 +25,15 @@ namespace preprocessor {
 	void reflect(tile& o, tile& t);
 	//function to compare if a tile is equal to another
 	bool is_equal(tile& a, tile& b);
+	//function to compare if an adj rule is equal to another
+	bool is_equal(adj_rule& a, adj_rule& b);
+	//function to check if an adj rule is already in the rule list
+	bool is_in(std::vector<adj_rule>& rules, adj_rule& rule);
 
-	//function to retrieve a list of tiles from a bitmap
-	void retrieve_tiles(std::vector<tile>& tiles, bitmap::bitmap bm);
+	//function to retrieve a list of tiles from a bitmap including a map of tiles, updating each one's adj and freq rules as well
+	void retrieve_tiles(std::vector<tile>& tiles, tile_pool& t_pool, bitmap::bitmap bm);
+	//function to retrieve a list of tiles' adjaceny rules according to a cached tile map
+	void retrieve_adj_rules(std::vector<tile>& tiles, tile_pool& t_pool);
 }
 
 #endif
